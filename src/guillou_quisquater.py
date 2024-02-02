69a597f86eafc2772eba80e1af78f4a477a8b61f
uwla <uwlalinux@gmail.com>
2024-02-02 20:44:24 -0300

    refactor: Guillou-Quisquater signature scheme

--- src/guillou_quisquater.py
+++ src/guillou_quisquater.py
@@ -1,48 +1,51 @@
 #!/usr/bin/python3
 from cripto_utils import concat, crt, exp_mod, mod_inv, prime_gt, prime_lt, rand_inv
+from cripto_types import SignatureScheme
 from random import randint
 
-# placeholder/stub for an actual hashing function
-def hash(x):
-    return exp_mod(x, 7, 1373)
+class GuillouQuisquater(SignatureScheme):
+    # Mocking an actual hashing function.
+    # For learning purposes, it does not matter too much the implementation of the hashing.
+    def hash(self, x):
+        return exp_mod(x, 7, 1373)
 
-def gq_genkeys(seclevel=1000):
-    p = prime_gt(seclevel)
-    q = prime_lt(seclevel)
-    n = p*q
-    phi_n = (p-1)*(q-1)
-    j = rand_inv(n)
-    e = rand_inv(phi_n)
-    jinv = mod_inv(j, n)
-    d1 = mod_inv(e, p-1)
-    d2 = mod_inv(e, q-1)
-    s1 = exp_mod(jinv, d1, p)
-    s2 = exp_mod(jinv, d2, q)
-    s = int(crt([p, q], [s1, s2]))
-    return ((n, e, j), s)
+    def generate_secrete_key(self):
+        p = prime_gt(self.sec_level)
+        q = prime_lt(self.sec_level)
+        n = p*q
+        phi_n = (p-1)*(q-1)
+        j = rand_inv(n)
+        e = rand_inv(phi_n)
+        j_inv = mod_inv(j, n)
+        d1 = mod_inv(e, p-1)
+        d2 = mod_inv(e, q-1)
+        s1 = exp_mod(j_inv, d1, p)
+        s2 = exp_mod(j_inv, d2, q)
+        s = int(crt([p, q], [s1, s2]))
+        pub_key = (n, e, j)
+        return (s, pub_key)
+    
+    def derive_public_key(self, sec_key):
+        s, pub_key = sec_key
+        return pub_key
+    
+    def sign(self, msg, sec_key):
+        x = msg
+        s, (n, e, j) = sec_key
+        k = randint(1, 10000)
+        r = exp_mod(k, e, n)
+        t = self.hash(concat(x, r))
+        a = (k*exp_mod(s, t, n)) % n
+        return (a, t)
 
-def gq_sign(keys, x):
-    (n, e, j), s = keys
-    k = randint(1, 10000)
-    r = exp_mod(k, e, n)
-    t = hash(concat(x, r))
-    a = (k*exp_mod(s, t, n)) % n
-    return (a, t)
+    def verify(self, msg, pub_key, signature):
+        x = msg
+        n, e, j = pub_key
+        a, t = signature
+        u = exp_mod(a, e, n) * exp_mod(j, t, n) % n
+        h = self.hash(concat(x, u))
+        return t == h
 
-def gq_verify(pkey, x, signature):
-    n, e, j = pkey
-    a, t = signature
-    u = exp_mod(a, e, n) * exp_mod(j, t, n) % n
-    t_ = hash(concat(x, u))
-    return t == t_
-
-def test_gq_signature():
-    keys = gq_genkeys()
-    pkey = keys[0]
-    p = pkey[0]
-    x = randint(1,p-1)
-    signature = gq_sign(keys, x)
-    verification = gq_verify(pkey, x, signature)
-    print(verification, x, signature)
-
-test_gq_signature()
+if __name__ == "__main__":
+    guillou_quisquater_scheme = GuillouQuisquater(10000)
+    guillou_quisquater_scheme.test_signature()
